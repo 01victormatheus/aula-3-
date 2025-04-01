7ffc171f8fd755d80279a1572d1b7bd4ede5834b
victor matheus <vtinnmatheus@gmail.com>
2025-04-01 09:18:33 -0300

    adicionei o percentual de aumento salarial

--- arquivo01.py
+++ arquivo01.py
@@ -1,4 +1,8 @@
 nome=input("digite seu nome")
 idade=int(input("digite sua idade"))
 salario=float(input("digite seu salario"))
-print (f"seu nome é {nome}, sua idade é {idade} , seu salario é {salario}" )
+porcentagem=float(input("digite a porcentagem"))
+aumento=(salario*porcentagem)/100
+total=salario+aumento
+
+print (f"seu nome é {nome}, sua idade é {idade} , seu salario é {salario} ,sua porcentagem salarial foi de {porcentagem :.2f},seu aumento salarial foi de {aumento} , seu total é de {total}" )
