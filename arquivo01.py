a5e42139fd72a086e44feeaa1c2f9cbe5f9d46fc
victor matheus <vtinnmatheus@gmail.com>
2025-04-01 08:50:18 -0300

    nome do usuario,idade,salario

---
+++ arquivo01.py
@@ -0,0 +1,4 @@
+nome=input("digite seu nome")
+idade=int(input("digite sua idade"))
+salario=float(input("digite seu salario"))
+print (f"seu nome é {nome}, sua idade é {idade} , seu salario é {salario}" )
