4ecc9bdc9745a540a50529bef957dd5db6decb02
victor matheus <vtinnmatheus@gmail.com>
2025-04-01 09:47:53 -0300

    quatro operaçoes

---
+++ arquivo 02.py
@@ -0,0 +1,9 @@
+n1=float(input("digite um numero"))
+n2=float(input("digite um numero"))
+adiçao=n1+n2
+subtraçao=n1-n2
+multiplicaçao=n1*n2
+divisao=n1/n2
+
+print(adiçao,subtraçao,multiplicaçao,divisao)
+
